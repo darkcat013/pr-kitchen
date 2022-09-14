4a2782798a6efdefa65511ad7c77c82d90422b21
darkcat013 <33005571+darkcat013@users.noreply.github.com>
2022-09-14 11:50:20 +0300

    Add order preparation logic, change from gin to mux (#3)

---
+++ domain/food.go
@@ -0,0 +1,9 @@
+package domain
+
+type Food struct {
+	Id               int    `json:"id"`
+	Name             string `json:"name"`
+	PreparationTime  int    `json:"preparation-time"`
+	Complexity       int    `json:"complexity"`
+	CookingApparatus string `json:"cooking-apparatus"`
+}
